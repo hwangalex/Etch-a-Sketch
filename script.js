d05d3795915ab02190e8e28167b41bdb347006c1
Alex Hwang <alexhwang01134@gmail.com>
2023-10-04 10:09:04 -0400

    Implemented a createGrid function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const gridContainer = document.getElementById("gridContainer");
+const gridContainer = document.getElementById('gridContainer');
+//const squareDiv = document.querySelector('gridSquare');
 
 function createGrid(width, height) {
     for (i = 0; i < width; i++) {
@@ -6,8 +7,22 @@ function createGrid(width, height) {
             const square = document.createElement('div');
             square.classList.add('gridSquare');
             gridContainer.appendChild(square);
+    
+            square.addEventListener('mouseenter', () => {
+                square.style.backgroundColor = 'blue';
+            });
+    
+            square.addEventListener('mouseleave', () => {
+                square.style.backgroundColor = 'white';
+            });
+    
         }
     }
 }
 
-createGrid(16, 16);
+
+createGrid(16,16);
+
+//button -> user clicks the button and a pop up will show up to let the user choose a color
+//          and so the set color will be that color and wheneve you hover over a square, it
+//          makes it that color
